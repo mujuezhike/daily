a24c84de890903a179e59bde98a78f2228ca7d3a
雷霆UPC <UPC@DESKTOP-0QNAO9B>
2017-09-19 09:41:19 +0800

    card game

--- src/main/java/com/mujuezhike/daily/card/CardEnums.java
+++ src/main/java/com/mujuezhike/daily/card/CardEnums.java
@@ -2,6 +2,16 @@ package com.mujuezhike.daily.card;
 
 public enum CardEnums{
 
+	VOID            (0,"虚空"),
+	WATER           (1,"海水"),
+	FIRE            (2,"火焰"),
+	EARTH           (3,"大地"),
+	TREE            (4,"森林"),
+	WIND            (5,"暴风"),
+	METAL           (6,"金属"),
+	LIGHT           (7,"光明"),
+	DARK            (8,"黑暗"),
+	CHAOS           (9,"混沌"),
 	;
 
 	/**
